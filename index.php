5c87b1b307f13318ea251c533e993d041c30534f
Mohana Prabha Palanisamy <71735755+MohanaPrabha16@users.noreply.github.com>
2022-02-06 22:20:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ try {
     $sql = "SELECT TOP 1 * FROM earthquake";
    
     // Executes the query
-    $stmt = $conn->query("$sql");
+    $stmt = $conn->query($sql);
     $row = $stmt->fetch();
     echo "$row[0] $row[1] $row[2]";
 //      echo "<table border='1'>
