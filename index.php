8a1d0b7f006db439e79c3381610aa86e9629fdaf
Mohana Prabha Palanisamy <71735755+MohanaPrabha16@users.noreply.github.com>
2022-02-06 22:38:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,31 @@
 
 <body>
 
-
+<table>
+    <tr>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+        <th>col1</th>
+    </tr>
 <?php
 try {
     $serverName = "tcp:assignmentserver01.database.windows.net,1433";
@@ -18,13 +42,18 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Select Query
-    $sql = "SELECT TOP 1 * FROM earthquake";
+    $sql = "SELECT TOP 5 * FROM earthquake";
 
     // Executes the query
-    $stmt = $conn->query("$sql");
-    $row = $stmt->fetch();
-    echo "<th><tr>mag type</tr><tr>id</tr></th>"
-    echo "$row[0] $row[1] $row[2]";
+    $result = $conn->query("$sql");
+    if($result->nums_rows > 0){
+        while($row = $result-> fetch_assoc()){
+            echo "<tr><td>". $row["time"] ."<tr><td>". $row["latitude"] ."<tr><td>". $row["longitude"] ."<tr><td>". $row["depth"] ."<tr><td>". $row["mag"] ."<tr><td>". $row["magType"] ."<tr><td>". $row["nst"] ."<tr><td>". $row["gap"] ."<tr><td>". $row["dmin"] ."<tr><td>". $row["rms"] ."<tr><td>". $row["net"] ."<tr><td>". $row["id"] ."<tr><td>". $row["updated"] ."<tr><td>". $row["place"] ."<tr><td>". $row["type"] ."<tr><td>". $row["horizontalError"] ."<tr><td>". $row["depthError"] ."<tr><td>". $row["magError"] ."<tr><td>". $row["magNst"] ."<tr><td>". $row["status"] ."<tr><td>". $row["locationSource"] ."<tr><td>". $row["magSource"] ."</t
+    }
+    }
+    else{
+    echo "No Results";
+    }
     $conn=NULL;
 } catch (PDOException $exception1) {die(print_r($e));
 //     echo "<h1>Caught PDO exception:</h1>";
@@ -35,7 +64,7 @@ try {
 
 ?>
 
-
+    </table>
 
 </body>
 
