da1d3ad1e48c950ae1e4667e608d3fbd356011b4
Mohana Prabha Palanisamy <71735755+MohanaPrabha16@users.noreply.github.com>
2022-02-06 19:36:24 -0600

    Update index.php

--- index.php
+++ index.php
@@ -32,10 +32,9 @@
 </form>
 </fieldset>
 <?php
-    echo "ENTERED";
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:assignmentserver01.database.windows.net,1433; Database = adbserver", "admin1", "{Ajithsivadas#1}");
+    $conn = new PDO("sqlsrv:server = tcp:assignmentserver01.database.windows.net,1433; Database = adbserver", "admin1", "Ajithsivadas#1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -45,24 +44,9 @@ catch (PDOException $e) {
 
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "admin1", "pwd" => "{your_password_here}", "Database" => "adbserver", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    echo $connectionInfo;
 $serverName = "tcp:assignmentserver01.database.windows.net,1433";
-    echo $serverName;
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-    echo "CONNECTION SUCCESS";
-    $sql = "SELECT * FROM earthquake";
-    echo $sql;
-$result = $conn->query($sql);
-echo $result;
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Place: " . $row["place"]. " " . $row["type"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-?> 
+?>
 </body>
 
 </html>
