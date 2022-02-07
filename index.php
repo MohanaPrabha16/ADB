2fb69b8ae092aab78650045bf5c234f517844689
Mohana Prabha Palanisamy <71735755+MohanaPrabha16@users.noreply.github.com>
2022-02-06 21:48:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contact Form - PHP/MySQL Demo Code</title>
+</head>
+
+<body>
+
+
 <?php
-// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:assignmentserver01.database.windows.net,1433; Database = adbserver", "admin1", "{your_password_here}");
+    $serverName = "tcp:assignmentserver01.database.windows.net,1433";
+    $databaseName = "adbserver";
+    $uid = "admin1@assignmentserver01";
+    $pwd = "Ajithsivadas#1";
+    
+    $conn = new PDO("sqlsrv:server = $serverName; Database = $databaseName;", $uid, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+
+    // Select Query
+    $sql = "SELECT TOP 1 * FROM earthquake";
+
+    // Executes the query
+    $stmt = $conn->query("$sql");
+    $row = $stmt->fetch();
+    echo "$row[0] $row[1] $row[2]";
+    $conn=NULL;
+} catch (PDOException $exception1) {die(print_r($e));
+//     echo "<h1>Caught PDO exception:</h1>";
+//     echo $exception1->getMessage() . PHP_EOL;
+//     echo "<h1>PHP Info for troubleshooting</h1>";
+//     phpinfo();
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "admin1", "pwd" => "{your_password_here}", "Database" => "adbserver", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:assignmentserver01.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
+
+
+
+</body>
+
+</html>
