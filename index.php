10fdd4ed47b1180415ccaa64fe9f3eb47f99f3ec
Mohana Prabha Palanisamy <71735755+MohanaPrabha16@users.noreply.github.com>
2022-02-06 19:12:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 </form>
 </fieldset>
 </body>
-    <?php
+<!-- <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:assignmentserver01.database.windows.net,1433; Database = adbserver", "admin1", "{Ajithsivadas#1}");
@@ -51,15 +51,15 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     $sql = "SELECT * FROM [dbo].[earthquake]";
 $result = $conn->query($sql);
 echo $result;
-/*if ($result->num_rows > 0) {
+if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "id: " . $row["id"]. " - Place: " . $row["place"]. " " . $row["type"]. "<br>";
     }
 } else {
-    echo "0 results";*/
+    echo "0 results";
 }
-?>
+?> -->
 </html>
-?>
+
 
