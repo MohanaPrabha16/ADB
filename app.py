82cdb0893a5211da92140885cbaea69acd38b5ce
Mohana Prabha Palanisamy <71735755+MohanaPrabha16@users.noreply.github.com>
2022-02-09 14:08:13 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -5,16 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    array1=[]
-    conn = pymssql.connect(server='serveradb.database.windows.net', user='admin1@serveradb', password='Ajithsivadas#1', database='assignment1')
-    cursor = conn.cursor()
-    q1="select * from [dbo].[people]" 
-    cursor.execute(q1)
-    result=cursor.fetchall()
-    for i in result:
-        j=numpy.asarray(i)
-        array1.append(j)
-    return render_template('main.html',result=array1)
+    return render_template('main.html')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -22,15 +13,15 @@ def search():
         first_name = request.form.get("name")
         print(first_name)
         if first_name is not None:
-            q1 = "SELECT * FROM [dbo].[people] WHERE NAME = '" + first_name + "'"
+            q1 = "SELECT * FROM [dbo].[test] WHERE Person = '" + first_name + "'"
         else:
             print("Empty value")
-        conn = pymssql.connect(server='serveradb.database.windows.net', user='admin1@serveradb', password='Ajithsivadas#1', database='assignment1')
+        conn = pymssql.connect(server='serveradb.database.windows.net', user='admin1', password='Ajithsivadas#1', database='assignment1')
         cursor = conn.cursor()
         cursor.execute(q1)
         result=cursor.fetchall()
-        print(result[0][6])
-        return render_template('search_user.html', user=result[0][6])
+        print(result[0][2])
+        return render_template('search.html', picture=result[0][2],year=result[0][1])
 
     
 
