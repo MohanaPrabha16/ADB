cdab8d5d36d013fd18a93cca986a1b423b7dcfa7
Mohana Prabha Palanisamy <71735755+MohanaPrabha16@users.noreply.github.com>
2022-02-09 01:12:53 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import numpy as numpy
 import pymssql
 app = Flask(__name__)
@@ -14,6 +14,21 @@ def index():
     for i in result:
         j=numpy.asarray(i)
         array1.append(j)
-
     return render_template('main.html',result=array1)
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        first_name = request.form.get("name")
+        if first_name is not None:
+            q1 = "SELECT * FROM [dbo].[people] WHERE NAME = '" + first_name + "'"
+        else:
+            print("Empty value")
+        conn = pymssql.connect(server='serveradb.database.windows.net', user='admin1@serveradb', password='Ajithsivadas#1', database='assignment1')
+        cursor = conn.cursor()
+        cursor.execute(q1)
+        result=cursor.fetchall()
+        return render_template('search.html', user=result[6])
+
+    
 
